68b2958df2373c8105896c215e944d30f10d7b05
JuUrrutia <ju.urrutia@duocuc.cl>
2023-06-10 10:14:09 -0400

    diseño actualizado

--- scripts/formulario.js
+++
@@ -1,7 +0,0 @@
-function updateCharacterCount(element) {
-    const maxLength = 150;
-    const currentLength = element.value.length;
-    const remainingCharacters = maxLength - currentLength;
-    const counterElement = document.getElementById('contador-caracteres');
-    counterElement.textContent = remainingCharacters + ' caracteres restantes';
-  }
